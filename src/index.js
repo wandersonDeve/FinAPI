61c5bdfc87307ff77baf18766f343d79e01fef9f
Wanderson Santos <wanderson.santos@gcbinvestimentos.com>
2022-05-17 07:33:30 -0300

    feat(index): create account

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.PORT || 3333;
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Esta fucnionando" });
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ error: "Custumers already exist" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return res.status(201).send();
 });
 
 app.listen(port, () => {
