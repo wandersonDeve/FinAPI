23acd0620195997544c2de43622057779e2bf9b6
Wanderson Santos <wanderson.santos@gcbinvestimentos.com>
2022-05-17 08:53:56 -0300

    feat(index): create withdraw route

--- src/index.js
+++ src/index.js
@@ -23,6 +23,18 @@ function verifyIfExistsAccountCPF(req, res, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
 
@@ -60,9 +72,30 @@ app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
     type: "credit",
   };
 
-  customer.statement.push(statementOperations)
+  customer.statement.push(statementOperations);
 
-  return res.status(201).send()
+  return res.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
+  const { amount } = req.body;
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return res.status(400).json({ error: "Insufficient funds" });
+  }
+
+  const statementOperations = {
+    amount,
+    createdAt: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperations);
+
+  return res.status(201).send();
 });
 
 app.listen(port, () => {
