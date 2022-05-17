1ef660b6f06b4b8bd69d53fb9105c123e3ccaff5
Wanderson Santos <wanderson.santos@gcbinvestimentos.com>
2022-05-17 08:36:27 -0300

    feat(index): create deposit route

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const customers = [];
 
 //middleware
 function verifyIfExistsAccountCPF(req, res, next) {
-  const { cpf } = req.body;
+  const { cpf } = req.headers;
 
   const customer = customers.find((customer) => customer.cpf === cpf);
 
@@ -47,6 +47,22 @@ app.post("/account", (req, res) => {
 app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
   const { customer } = req;
   return res.status(200).json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
+  const { description, amount } = req.body;
+  const { customer } = req;
+
+  const statementOperations = {
+    description,
+    amount,
+    createdAt: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperations)
+
+  return res.status(201).send()
 });
 
 app.listen(port, () => {
